9c612a0e5ce5c453891e1977185335ad8f8db79b
eve <evangelinale201@gmail.com>
2025-04-23 12:19:58 -0500

    added Vue.Js into the files, combined css file

--- mainpage-script.js
+++ mainpage-script.js
@@ -8,3 +8,19 @@ particleClick.addEventListener("click", (e) => {
         particleClick.classList.remove("animate")
     }, 600)
 } )
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            genres: [
+                { name: 'Pop', subgenres: ['Indie Pop', 'New Wave', 'Kpop', 'Pop-punk', 'Country Pop', 'Teen Pop'] },
+                { name: 'Rock', subgenres: ['Alt Rock', 'Grunge', 'Blues Rock', 'Soft Rock', 'Metal', 'Classic Rock'] },
+                { name: 'Jazz', subgenres: ['Blues', 'Funk', 'Dixieland', 'Soul Jazz', 'Bossa Nova', 'Bepop'] },
+                { name: 'Indie', subgenres: ['Shoegaze', 'Slowcore', 'Indietronica', 'Chamber Pop', 'Emo', 'Chillwave'] },
+                {name: 'Rap', subgenres: ['Drill', 'Trap', 'Hyphy', 'Consciious Rap', 'Boom-Bap', 'Alt Rap']}
+            ]
+        }
+    }
+}).mount('#app');
