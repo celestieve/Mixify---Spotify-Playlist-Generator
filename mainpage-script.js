80df15a4608e955ee1598b51dabfeb6761d2ffb4
LeviGColeman97 <LeviGColeman97@gmail.com>
2025-04-28 13:40:40 -0500

    Updates, fixed errors

--- mainpage-script.js
+++ mainpage-script.js
@@ -1,26 +1,42 @@
-const particleClick = document.querySelector(".button-particle-click");
-
-particleClick.addEventListener("click", (e) => {
-    e.preventDefault();
-    particleClick.classList.add("animate")
-
-    setTimeout(() => {
-        particleClick.classList.remove("animate")
-    }, 600)
-} )
-
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            genres: [
-                { name: 'Pop', subgenres: ['Indie Pop', 'New Wave', 'Kpop', 'Pop-punk', 'Country Pop', 'Teen Pop'] },
-                { name: 'Rock', subgenres: ['Alt Rock', 'Grunge', 'Blues Rock', 'Soft Rock', 'Metal', 'Classic Rock'] },
-                { name: 'Jazz', subgenres: ['Blues', 'Funk', 'Dixieland', 'Soul Jazz', 'Bossa Nova', 'Bepop'] },
-                { name: 'Indie', subgenres: ['Shoegaze', 'Slowcore', 'Indietronica', 'Chamber Pop', 'Emo', 'Chillwave'] },
-                {name: 'Rap', subgenres: ['Drill', 'Trap', 'Hyphy', 'Consciious Rap', 'Boom-Bap', 'Alt Rap']}
-            ]
-        }
-    }
+data() {
+return {
+genres: [
+{ name: 'Pop', subgenres: ['Indie Pop', 'New Wave', 'k-pop', 'Pop-punk', 'Country Pop', 'Teen Pop'] },
+{ name: 'Rock', subgenres: ['Alt Rock', 'Grunge', 'Blues Rock', 'Soft Rock', 'Metal', 'Classic Rock'] },
+{ name: 'Jazz', subgenres: ['Blues', 'Funk', 'Dixieland', 'Soul Jazz', 'Bossa Nova', 'Bepop'] },
+{ name: 'Indie', subgenres: ['Shoegaze', 'Slowcore', 'Indietronica', 'Chamber Pop', 'Emo', 'Chillwave'] },
+{ name: 'Rap', subgenres: ['Drill', 'Trap', 'Hyphy', 'Conscious Rap', 'Boom-Bap', 'Alt Rap'] }
+]
+};
+},
+methods: {
+selectSubgenre(subgenre) {
+console.log('Selected subgenre:', subgenre);
+localStorage.setItem('selectedGenre', subgenre);
+},
+goToGenerate() {
+const selectedGenre = localStorage.getItem('selectedGenre');
+if (!selectedGenre) {
+alert('Please select a subgenre first!');
+} else {
+location.href = 'generation.html';
+}
+}
+},
+mounted() {
+const particleClick = document.querySelector(".button-particle-click");
+if (particleClick) {
+particleClick.addEventListener("click", (e) => {
+e.preventDefault();
+particleClick.classList.add("animate");
+
+setTimeout(() => {
+particleClick.classList.remove("animate");
+}, 600);
+});
+}
+}
 }).mount('#app');
